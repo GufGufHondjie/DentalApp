951508d1eae793dbbd2958dd712543c6094566ca
R <rafbauduin@hotmail.com>
2025-05-25 09:47:25 -0400

    Code Style & Linting Enforcement

--- eslint.config.js
+++ eslint.config.js
@@ -26,3 +26,25 @@ export default tseslint.config(
     },
   },
 )
+
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'react/prop-types': 'off',
+    'no-console': 'warn'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+}
